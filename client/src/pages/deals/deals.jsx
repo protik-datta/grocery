5ed1a9e655b705227cde578312e8621af37c351a
Protik Datta <protikdatta135@gmail.com>
2026-05-09 01:22:21 +0600

    test

---
+++ client/src/pages/deals/deals.jsx
@@ -0,0 +1,9 @@
+const Deals = () => {
+  return (
+    <div className="flex flex-col items-start self-stretch py-10 px-23.75 bg-linear-to-r from-[#F97316] to-[#EA580C]">
+      dealss
+    </div>
+  );
+};
+
+export default Deals;
