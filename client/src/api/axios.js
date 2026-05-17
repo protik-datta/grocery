d77f20bf5b94a38c8a8012998cfbd060eb06e65a
Protik Datta <protikdatta135@gmail.com>
2026-05-18 01:51:27 +0600

    backend login register issue fix cors policy and same site none

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URI || "http://localhost:3000/api",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 10000,
 });
 
 api.interceptors.request.use(
--- server/src/helpers/generateToken.js
+++ server/src/helpers/generateToken.js
@@ -8,7 +8,7 @@ const setAuthCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
@@ -17,7 +17,7 @@ const clearAuthCookie = (res) => {
   res.clearCookie("token", "", {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "none",
   });
 };
 
